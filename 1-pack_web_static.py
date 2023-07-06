35d43d1f9e6080e491b85a669a2a590f32afa045
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-06 21:22:16 +0300

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
+
 from datetime import datetime
+from os.path import isdir
 
 from fabric.api import local
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
-    now = datetime.now()
-
-    # Create a formatted date string for the archive name
-    dt_string = now.strftime("web_static_%Y%m%d%H%m%S")
-
-    # Set the output file path and name
-    output_file = "versions/{:}.tgz".format(dt_string)
-
-    # Create the 'versions' directory if it doesn't exist
-    local("mkdir -p versions")
-
-    # Compress the 'web_static' folder into the output file
-    result = local("tar -cvzf {:} web_static".format(output_file))
-
-    # Check if the compression was successful
-    if result.failed:
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Exception as e:
+        print("An error occurred:", str(e))
         return None
-
-    # Return the path to the created archive
-    return output_file
