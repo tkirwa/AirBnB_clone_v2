7cec790ab3a3d263b6650e455d058a8d5eb4a89c
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-07 02:46:40 +0300

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # Fabfile to delete out-of-date archives.
 import os
+
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+api.env.hosts = ["34.204.95.241", "52.87.230.196"]
 
 
 def do_clean(number=0):
