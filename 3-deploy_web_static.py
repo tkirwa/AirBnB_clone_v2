8faa8e978faf2735c3a166af3fab09343eb441db
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-07 02:34:20 +0300

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -33,15 +33,15 @@ def do_deploy(archive_path):
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
+        path = "~/data/web_static/releases/"
         put(archive_path, "/tmp/")
         run("mkdir -p {}{}/".format(path, no_ext))
         run("tar -xzf /tmp/{} -C {}{}/".format(file_n, path, no_ext))
         run("rm /tmp/{}".format(file_n))
         run("mv {0}{1}/web_static/* {0}{1}/".format(path, no_ext))
         run("rm -rf {}{}/web_static".format(path, no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, no_ext))
+        run("rm -rf ~/data/web_static/current")
+        run("ln -s {}{}/ ~/data/web_static/current".format(path, no_ext))
         return True
     except Exception as e:
         print("An error occurred:", str(e))
