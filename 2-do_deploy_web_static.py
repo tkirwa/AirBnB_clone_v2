d9e5c0ad2241742e0a6839873d3b22da94819617
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-09 22:46:23 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,51 +1,69 @@
 #!/usr/bin/python3
-"""
-A Fabric script that distributes an archive to your web servers,
-using the function do_deploy
-"""
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-from fabric.api import *
-from datetime import datetime
-from os.path import exists
-
-
-env.hosts = ["34.204.95.241", "52.87.230.196"]  # <IP web-01>, <IP web-02>
+env.hosts = ["34.204.95.241", "52.87.230.196"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if not exists(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
         return False
 
-    try:
-        file_name = archive_path.split('/')[-1]
-        folder_name = file_name.split('.')[0]
-        release_path = "/data/web_static/releases/{}".format(folder_name)
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/")
-        
-        # Uncompress the archive to the folder /data/web_static/releases/
-        # <archive filename without extension> on the web server
-        run("mkdir -p {}".format(release_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, release_path))
-        
-        # Remove the uploaded archive from the web server
-        run("rm /tmp/{}".format(file_name))
-        
-        # Move the contents of the uncompressed folder to the release folder
-        run("mv {}/web_static/* {}".format(release_path, release_path))
-        
-        # Remove the unnecessary web_static folder
-        run("rm -rf {}/web_static".format(release_path))
-        
-        # Remove the current symbolic link
-        run("rm -rf /data/web_static/current")
-        
-        # Create a new symbolic link to the new release folder
-        run("ln -s {} /data/web_static/current".format(release_path))
-        
-        return True
-    except Exception as e:
-        print("An error occurred:", str(e))
+    # Upload the archive to the /tmp/ directory of the web server
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+
+    # Remove existing release folder
+    if run("rm -rf /data/web_static/releases/{}/"
+           .format(name)).failed is True:
+        return False
+
+    # Create the release directory
+    if run("mkdir -p /data/web_static/releases/{}/"
+           .format(name)).failed is True:
+        return False
+
+    # Uncompress the archive to the release directory
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+           .format(file, name)).failed is True:
+        return False
+
+    # Remove the uploaded archive from the web server
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+
+    # Move the contents of the uncompressed folder to the release directory
+    mv_command = "mv /data/web_static/releases/{}/web_static/* " \
+        "/data/web_static/releases/{}/".format(name, name)
+    if run(mv_command).failed is True:
+        return False
+    
+    # Remove the empty web_static directory
+    if run("rm -rf /data/web_static/releases/{}/web_static"
+           .format(name)).failed is True:
+        return False
+
+    # Remove the current symbolic link
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+
+    # Create a new symbolic link to the new release folder
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+           .format(name)).failed is True:
+        return False
+
+    return True
