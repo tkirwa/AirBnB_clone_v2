b0d8f6a8c62127b285c07aeec0ea6e2f3b3d57d1
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-09 23:54:59 +0300

    101-setup_web_static.pp fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -6,12 +6,12 @@ $nginx_conf = "server {
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
-    location /hbnb_static {
-        alias /home/ubuntu/data/web_static/current;
+    location /hbnb_static/ {
+        alias /data/web_static/current;
         index index.html index.htm;
     }
     location /redirect_me {
-        return 301 http://linktr.ee/firdaus_h_salim/;
+        return 301 https://www.google.com/;
     }
     error_page 404 /404.html;
     location /404 {
@@ -25,37 +25,37 @@ package { 'nginx':
   provider => 'apt'
 }
 
--> file { '/home/ubuntu/data':
+-> file { '/data':
   ensure  => 'directory'
 }
 
--> file { '/home/ubuntu/data/web_static':
+-> file { '/data/web_static':
   ensure => 'directory'
 }
 
--> file { '/home/ubuntu/data/web_static/releases':
+-> file { '/data/web_static/releases':
   ensure => 'directory'
 }
 
--> file { '/home/ubuntu/data/web_static/releases/test':
+-> file { '/data/web_static/releases/test':
   ensure => 'directory'
 }
 
--> file { '/home/ubuntu/data/web_static/shared':
+-> file { '/data/web_static/shared':
   ensure => 'directory'
 }
 
--> file { '/home/ubuntu/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "this webpage is found in data/web_static/releases/test/index.htm \n"
 }
 
--> file { '/home/ubuntu/data/web_static/current':
+-> file { '/data/web_static/current':
   ensure => 'link',
-  target => '/home/ubuntu/data/web_static/releases/test'
+  target => '/data/web_static/releases/test'
 }
 
--> exec { 'chown -R ubuntu:ubuntu /home/ubuntu/data/':
+-> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
