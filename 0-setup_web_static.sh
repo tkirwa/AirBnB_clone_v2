311008924c02322b7951860dc64c898321c83b33
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-09 23:30:59 +0300

    2-do_deploy_web_static.py

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,32 @@
 #!/usr/bin/env bash
+# sets up the web servers for the deployment of web_static
 
-# Install Nginx if it's not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
+sudo apt-get -y update
+# Install Nginx if it is not already installed
+sudo apt-get -y install nginx
+# Create the folder /data/ if it doesn’t already exist
+# Create the folder /data/web_static/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-# Create necessary folders if they don't exist
-web_static_dir="/data/web_static"
-releases_dir="$web_static_dir/releases"
-shared_dir="$web_static_dir/shared"
-test_dir="$releases_dir/test"
-index_file="$test_dir/index.html"
-
-sudo mkdir -p "$web_static_dir"
-sudo mkdir -p "$releases_dir"
-sudo mkdir -p "$shared_dir"
-sudo mkdir -p "$test_dir"
-
-# Create a fake HTML file for testing
+# echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > "$index_file"
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or update symbolic link
-symbolic_link="/data/web_static/current"
-if [ -L "$symbolic_link" ]; then
-    sudo rm "$symbolic_link"
-fi
-sudo ln -s "$test_dir" "$symbolic_link"
-
-# Give ownership to ubuntu user and group
-sudo chown -R ubuntu:ubuntu "/data/"
-
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-sudo sed -i 's/^\(\s*location \/ {\)$/\1\n\t\talias \/data\/web_static\/current\/;/' "$config_file"
-
-# Restart Nginx
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# The command chown -hR is used to change the ownership of files and directories recursively
+sudo chown -hR ubuntu:ubuntu /data/
+# This command inserts the following block of text at line 38 of the
+# /etc/nginx/sites-available/default file
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Update the Nginx configuration to serve the content of
+# /data/web_static/current/ to hbnb_static
+# Restart Nginx after updating the configuration
 sudo service nginx restart
-
-exit 0
