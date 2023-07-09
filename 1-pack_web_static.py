c31aef313a3edb3a4ddb72c83aefca11769bb7fa
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-09 21:09:03 +0300

    1-pack_web_static.py - updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-""" This module contains the function do_pack that generates a .tgz archive
-  from the contents of the web_static folder (fabric script) """
-
+"""This module contains the function do_pack that generates a .tgz archive
+from the contents of the web_static folder (fabric script)"""
 
 from fabric.api import *
 from datetime import datetime
 
 
 def do_pack():
-    """ Fabric script that generates a .tgz archive from the contents of the...
-    ...web_static folder """
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_{}.tgz".format(date)
-    result = local("tar -cvzf {} web_static".format(filename))
-    if result.succeeded:
-        return filename
-    else:
+    """Fabric script that generates a .tgz archive from the contents of the web_static folder"""
+    try:
+        local("mkdir -p versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = "versions/web_static_{}.tgz".format(date)
+        result = local("tar -cvzf {} web_static".format(filename))
+        if result.succeeded:
+            return filename
+        else:
+            return None
+    except Exception as e:
+        print("An error occurred:", str(e))
         return None
