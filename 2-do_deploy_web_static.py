76ce82b9ac96638bd35fb800141647e21fb35f7c
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-09 21:55:36 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
-"""Deploy an archive of static html to my web servers with Fabric3"""
+"""Fabric script that distributes an archive to your web servers"""
 
-import os
+from datetime import datetime
+from os.path import exists
 
-from fabric import api
-from fabric.contrib import files
+from fabric.api import env, put, run, task
 
-api.env.hosts = ["34.204.95.241", "52.87.230.196"]
-api.env.user = "ubuntu"
-api.env.key_filename = "~/.ssh/school"
+env.hosts = ["34.204.95.241", "52.87.230.196"]
+env.user = "ubuntu"
 
 
+@task
 def do_deploy(archive_path):
-    """Function to transfer `archive_path` to web servers.
-
-    Args:
-        archive_path (str): path of the .tgz file to transfer
-
-    Returns: True on success, False otherwise.
-    """
-    if not os.path.isfile(archive_path):
+    """Distributes an archive to your web servers"""
+    if not exists(archive_path):
         return False
-    with api.cd("/tmp"):
-        basename = os.path.basename(archive_path)
-        root, ext = os.path.splitext(basename)
-        outpath = "~/data/web_static/releases/{}".format(root)
-        try:
-            putpath = api.put(archive_path)
-            if files.exists(outpath):
-                api.run("rm -rdf {}".format(outpath))
-            api.run("mkdir -p {}".format(outpath))
-            api.run("tar -xzf {} -C {}".format(putpath[0], outpath))
-            api.run("rm -f {}".format(putpath[0]))
-            api.run("mv -u {}/web_static/* {}".format(outpath, outpath))
-            api.run("rm -rf {}/web_static".format(outpath))
-            api.run("rm -rf ~/data/web_static/current")
-            api.run("ln -sf {} ~/data/web_static/current".format(outpath))
-            print("New version deployed!")
-        except Exception as e:
-            print("An error occurred:", str(e))
-            return False
-        else:
-            return True
+    try:
+        file_name = archive_path.split("/")[-1]
+        folder_name = file_name.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}".format(folder_name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, path_no_ext))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}/web_static".format(path_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(path_no_ext))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
+
+
+def do_pack():
+    """Generates a .tgz archive"""
+    try:
+        from fabric.operations import local
+
+        local("mkdir -p versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Exception:
+        return None
