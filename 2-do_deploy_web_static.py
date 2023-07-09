b3e4a369b8ef30188b11b265c587677cbca8753b
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-09 22:19:41 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,51 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers"""
+"""
+A Fabric script that distributes an archive to your web servers,
+using the function do_deploy
+"""
 
+from fabric.api import *
 from datetime import datetime
 from os.path import exists
 
-from fabric.api import env, put, run, task
 
-env.hosts = ["34.204.95.241", "52.87.230.196"]
-env.user = "ubuntu"
+env.hosts = ["34.204.95.241", "52.87.230.196"]  # <IP web-01>, <IP web-02>
 
 
-@task
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
+    """Distributes an archive to the web servers"""
     if not exists(archive_path):
         return False
-    try:
-        file_name = archive_path.split("/")[-1]
-        folder_name = file_name.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}".format(folder_name)
 
+    try:
+        file_name = archive_path.split('/')[-1]
+        folder_name = file_name.split('.')[0]
+        release_path = "/data/web_static/releases/{}".format(folder_name)
+
+        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, path_no_ext))
+        
+        # Uncompress the archive to the folder /data/web_static/releases/
+        # <archive filename without extension> on the web server
+        run("mkdir -p {}".format(release_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, release_path))
+        
+        # Remove the uploaded archive from the web server
         run("rm /tmp/{}".format(file_name))
-        run("mv {}/web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}/web_static".format(path_no_ext))
+        
+        # Move the contents of the uncompressed folder to the release folder
+        run("mv {}/web_static/* {}".format(release_path, release_path))
+        
+        # Remove the unnecessary web_static folder
+        run("rm -rf {}/web_static".format(release_path))
+        
+        # Remove the current symbolic link
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(path_no_ext))
-        print("New version deployed!")
+        
+        # Create a new symbolic link to the new release folder
+        run("ln -s {} /data/web_static/current".format(release_path))
+        
         return True
-    except Exception:
+    except Exception as e:
+        print("An error occurred:", str(e))
         return False
-
-
-def do_pack():
-    """Generates a .tgz archive"""
-    try:
-        from fabric.operations import local
-
-        local("mkdir -p versions")
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception:
-        return None
