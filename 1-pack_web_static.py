8f7becf5b0b0c814b850d6c85b6a397968f36ac4
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-09 21:10:22 +0300

    1-pack_web_static.py - updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,8 @@ from datetime import datetime
 
 
 def do_pack():
-    """Fabric script that generates a .tgz archive from the contents of the web_static folder"""
+    """Fabric script that generates a .tgz archive from the contents
+      of the web_static folder"""
     try:
         local("mkdir -p versions")
         date = datetime.now().strftime("%Y%m%d%H%M%S")
