4cf94b4a89370ece24419a769085632290df282d
Tonny Kirwa <118567738+tkirwa@users.noreply.github.com>
2023-07-09 13:38:25 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -15,9 +15,9 @@ def do_pack():
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
-            local("mkdir versions")
+            local("sudo mkdir -p versions")
         file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
+        local("sudo tar -cvzf {} web_static".format(file_name))
         return file_name
     except Exception as e:
         print("An error occurred:", str(e))
