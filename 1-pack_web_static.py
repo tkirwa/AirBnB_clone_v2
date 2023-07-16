d58dcd700a0fcad0c3b3fd4c6ef30abccc7cc123
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-16 22:34:11 +0300

    1-pack_web_static.py - include all files and subdirectories inside the web_static folder in the archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,18 +2,20 @@
 """This module contains the function do_pack that generates a .tgz archive
 from the contents of the web_static folder (fabric script)"""
 
-from fabric.api import *
 from datetime import datetime
+
+from fabric.api import *
 
 
 def do_pack():
     """Fabric script that generates a .tgz archive from the contents
-      of the web_static folder"""
+    of the web_static folder"""
     try:
         local("mkdir -p versions")
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         filename = "versions/web_static_{}.tgz".format(date)
-        result = local("tar -cvzf {} web_static".format(filename))
+        result = local("tar -cvzf {} web_static/*".format(filename))
+
         if result.succeeded:
             return filename
         else:
