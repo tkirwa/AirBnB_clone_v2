d97566ec5623b23e4eff9c69d193dc2fad7af339
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-16 22:43:27 +0300

    1-pack_web_static.py - include all files and subdirectories inside the web_static folder in the archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""This module contains the function do_pack that generates a .tgz archive
-from the contents of the web_static folder (fabric script)"""
-
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
 
-from fabric.api import *
+from fabric.api import local
 
 
 def do_pack():
-    """Fabric script that generates a .tgz archive from the contents
-    of the web_static folder"""
-    try:
-        local("mkdir -p versions")
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        filename = "versions/web_static_{}.tgz".format(date)
-        # result = local("tar -cvzf {} web_static/*".format(filename))
-        result = local("tar -cvzf {} -C web_static .".format(filename))
-
-        if result.succeeded:
-            return filename
-        else:
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+    )
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
             return None
-    except Exception as e:
-        print("An error occurred:", str(e))
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
