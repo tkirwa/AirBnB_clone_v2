a4d65cf4dde7f7138dc57bef7970166271a627c3
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-16 22:39:34 +0300

    1-pack_web_static.py - include all files and subdirectories inside the web_static folder in the archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,7 +14,8 @@ def do_pack():
         local("mkdir -p versions")
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         filename = "versions/web_static_{}.tgz".format(date)
-        result = local("tar -cvzf {} web_static/*".format(filename))
+        # result = local("tar -cvzf {} web_static/*".format(filename))
+        result = local("tar -cvzf {} -C web_static .".format(filename))
 
         if result.succeeded:
             return filename
