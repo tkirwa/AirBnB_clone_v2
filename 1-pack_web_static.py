309e77fdfcc45a43fa2a7c501d1cc25bfb83b5cd
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-21 23:42:29 +0300

    2-do_deploy_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,55 +7,21 @@ from datetime import datetime
 from fabric.api import *
 
 
-def do_deploy(archive_path):
-    """Deploy web files to server"""
+def do_pack():
+    """Fabric script that generates a .tgz archive from the contents
+    of the web_static folder"""
     try:
-        if not path.exists(archive_path):
-            return False
+        local("mkdir -p versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(date)
+        # result = local("tar -cvzf {} -C web_static".format(filename))
+        # result = local("tar -cvzf {} -C web_static/*".format(archive_path))
+        result = local("tar -cvzf {} web_static".format(archive_path))
 
-        # Upload archive
-        print("Uploading archive...")
-        put(archive_path, '/tmp/')
-
-        # Get the filename without the extension
-        filename = path.basename(archive_path).split(".")[0]
-
-        # Create target dir
-        print("Creating target directory...")
-        run('mkdir -p /data/web_static/releases/{}/'.format(filename))
-
-        # Uncompress archive and delete .tgz
-        print("Uncompressing archive...")
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-            .format(filename, filename))
-
-        # Remove archive
-        print("Removing archive...")
-        run('rm /tmp/{}.tgz'.format(filename))
-
-        # Move contents into host web_static
-        print("Moving contents to target directory...")
-        run('mv -n /data/web_static/releases/{}/web_static/*'
-            ' /data/web_static/releases/{}/'
-            .format(filename, filename))
-
-        # Remove extraneous web_static dir
-        print("Cleaning up extraneous directory...")
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(filename))
-
-        # Delete pre-existing symbolic link
-        print("Removing existing symbolic link...")
-        run('rm -rf /data/web_static/current')
-
-        # Re-establish symbolic link
-        print("Creating new symbolic link...")
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(filename))
-
-        print("New version deployed!")
-        # Return True on success
-        return True
+        if result.succeeded:
+            return archive_path
+        else:
+            return None
     except Exception as e:
         print("An error occurred:", str(e))
-        return False
+        return None
