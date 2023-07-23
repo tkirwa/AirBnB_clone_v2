a7581e1758c48991cb0e2576157a4509197412ad
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-24 00:32:51 +0300

    web_flask/100-hbnb.py, web_flask/templates/100-hbnb.html, web_flask/static/

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -4,6 +4,7 @@ from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 
 
 app = Flask(__name__)
@@ -15,7 +16,7 @@ def tear_down(self):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def show_page():
     """displays webpage
     Returns:
@@ -23,15 +24,19 @@ def show_page():
     """
     dict_states = storage.all(State)
     dict_amenities = storage.all(Amenity)
+    dict_places = storage.all(Place)
     all_states = []
     all_amenities = []
+    all_places = []
 
     for k, v in dict_states.items():
         all_states.append(v)
     for k, v in dict_amenities.items():
         all_amenities.append(v)
-    return render_template('10-hbnb_filters.html', all_states=all_states,
-                           all_amenities=all_amenities)
+    for k, v in dict_places.items():
+        all_places.append(v)
+    return render_template('100-hbnb.html', all_states=all_states,
+                           all_amenities=all_amenities, all_places=all_places)
 
 
 if __name__ == "__main__":
