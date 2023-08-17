c29d8fedeb568ce32edfef773c825a30974768e0
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-17 10:48:06 +0300

    0-hello_route.py -fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """
-This script starts a Flask web application with a single route.
+Script that starts a Flask web application
 """
-
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
-@app.route("/airbnb-onepage/", strict_slashes=False)
-def hello_hbnb():
-    """
-    Display "Hello HBNB!" when accessing the /airbnb-onepage/ route.
-    """
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
