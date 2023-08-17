7fc0428464a6146aa4cffac03466d3786e1ad82a
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-17 10:39:15 +0300

    airbnb-onepage route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,13 +8,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """
-    Display "Hello HBNB!" when accessing the root route.
+    Display "Hello HBNB!" when accessing the /airbnb-onepage/ route.
     """
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
