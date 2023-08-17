1c241879e4fe431c0a7c100383afdfe2a5baa7be
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-17 11:01:59 +0300

    airbnb-onepage route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application
+This script starts a Flask web application with a single route.
 """
-from flask import Flask
 
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return 'Hello HBNB!'
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Display "Hello HBNB!" when accessing the /airbnb-onepage/ route.
+    """
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
