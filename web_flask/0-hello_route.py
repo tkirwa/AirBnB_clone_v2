aa6e566cce2713c5acbd7877638dcb2b2afac8ae
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-18 18:41:31 +0300

    commented out some code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,20 +8,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# @app.route('/', strict_slashes=False)
-# def hello():
-#     """
-#     Display "Hello HBNB!" when accessing the / route.
-#     """
-#     return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Display "Hello HBNB!" when accessing the / route.
+    """
+    return 'Hello HBNB!'
 
 
-@app.route("/airbnb-onepage/", strict_slashes=False)
-def hello_hbnb():
-    """
-    Display "Hello HBNB!" when accessing the /airbnb-onepage/ route.
-    """
-    return "Hello HBNB!"
+# @app.route("/airbnb-onepage/", strict_slashes=False)
+# def hello_hbnb():
+#     """
+#     Display "Hello HBNB!" when accessing the /airbnb-onepage/ route.
+#     """
+#     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
